37085bf27dea07971addae908286cbe0db84b7f9
scjpdba <scjpdba@naver.com>
2022-07-29 15:00:27 +0900

    router 재설정

--- server/routes/router.js
+++
@@ -1,8 +0,0 @@
-const express = require('express'); const os = require('os');
-const router = express.Router();
-
-/* GET home page. */
-router.get('/', (req, res, next) => {
-    res.send('Server Response Success:router');
-});
-module.exports = router;
