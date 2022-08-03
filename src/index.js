875efa41198eb342a31edb1811e40c22a1ef0f43
scjpdba <scjpdba@naver.com>
2022-08-03 11:41:39 +0900

    달력출력

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './styles/_style.scss';
+import './styles/_common.scss';
+import './styles/_theme.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
